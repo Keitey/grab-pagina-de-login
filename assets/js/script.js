a5ee23b84231e076fc6269c47a83ee63b7a2b7bb
keitey <keiteybarros@gmail.com>
2022-02-14 01:54:53 -0300

    commit initial

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let email = id('email'),
+    password = id('password'),
+    form = id('form'),
+    favIcon = classes('fa-solid'),
+    errorMsg = classes('error');
+
+    form.addEventListener('submit', (error) => {
+        error.preventDefault();
+
+        validation(email, 0, 'Por favor, insira um email válido');
+        validation(password, 1, 'Por favor, insira uma senha válida');
+    });
+
+    let validation = (id, serial, message) => {
+
+        if(id.value.trim() === '') {
+            errorMsg[serial].innerHTML = message;
+            id.style.border = '2px solid #c21a1a';
+            favIcon[serial].style.color = '#c21a1a';
+        }else {
+            errorMsg[serial].innerHTML = '';
+            id.style.border = '2px solid #79f26e';
+            favIcon[serial].style.color = '#79f26e';
+        }
+    };
